b4dd4fe4c2a28b018f2b5779ca3c7d28a80d8540
ruolantang <ruolantang007@gmail.com>
2018-04-10 17:20:04 -0400

    edge detection, camera post effects script, download a sample scene.

---
+++ CS224NPR/Assets/script/postEffects.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;  
+using System.Collections;  
+
+[ExecuteInEditMode]  
+public class postEffects : MonoBehaviour {  
+
+	#region Variables  
+	//public Shader curShader;  
+	public Material curMaterial;  
+	#endregion   
+
+	// Use this for initialization  
+	void Start () {  
+		
+	}  
+
+	void OnRenderImage (RenderTexture sourceTexture, RenderTexture destTexture){  
+			Graphics.Blit(sourceTexture, destTexture, curMaterial);   
+	}  
+
+	// Update is called once per frame  
+	void Update () {  
+
+	}  
+		 
+}  
